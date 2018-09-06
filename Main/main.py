0ab055d81ae8310338bf4a6acd3cd67d79869cc3
GehuaZhang <zhangehuazee@gmail.com>
2018-09-05 21:33:31 -0400

    update XML process

--- Main/main.py
+++ Main/main.py
@@ -11,11 +11,13 @@ from ImageProcess import imageProcess
 from Model.VGG16.PreTrained import pretrainVGG16
 
 
+
 # Modify to your own system path
 os.chdir(r"D:\CodeProjects\AliothAtlas\FireDetect")
-train_path = r"DataSet\Train"
+train_path = r"DataSet\other"
 
 train_data = imageProcess.load_batch(train_path) # Load batch of pictures
 #train_data = imageProcess.load_single(train_path) # Load a single picture
 
 pretrainVGG16.pretrain_vgg16(train_data)
+
